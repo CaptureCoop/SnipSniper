a0956806238cce5c2301646fd7557c9b0062bf0d
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-03-31 22:55:55 +0200

    Removed commented out code

--- src/main/java/io/wollinger/snipsniper/editorwindow/EditorWindowRender.java
+++ src/main/java/io/wollinger/snipsniper/editorwindow/EditorWindowRender.java
@@ -22,11 +22,6 @@ public class EditorWindowRender extends JPanel{
 		g.drawImage(editorWnd.overdraw, 0,0,this.getWidth(),this.getHeight(),this);
 		g.setColor(editorWnd.currentColor);
 		
-		//Later used for a fill tool.
-		//Vector2Int startPoint = editorWnd.mouseListener.startPoint;
-		//Vector2Int lastPoint = editorWnd.mouseListener.lastPoint;
-		//if(startPoint != null && lastPoint != null) g.fillRect(startPoint.x, startPoint.y, lastPoint.x - startPoint.x, lastPoint.y - startPoint.y);
-		
 		Vector2Int mousePos = editorWnd.listener.mousePos;
 		Vector2Int brushSize = new Vector2Int(editorWnd.sniperInstance.cfg.getInt("editorStampWidth"), editorWnd.sniperInstance.cfg.getInt("editorStampHeight"));
 		if(mousePos != null) g.fillRect(mousePos.x - brushSize.x/2, mousePos.y - brushSize.y/2, brushSize.x, brushSize.y);
