a31264eae860c217af6083577268d1d44ef7212d
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-04-03 17:41:06 +0200

    Added comment to InputContainer

--- src/main/java/io/wollinger/snipsniper/utils/InputContainer.java
+++ src/main/java/io/wollinger/snipsniper/utils/InputContainer.java
@@ -2,7 +2,7 @@ package io.wollinger.snipsniper.utils;
 
 public class InputContainer {
     private boolean[] keys = new boolean[9182];
-    private int mouseWheelDirection = 0; //0 = Not moving
+    private int mouseWheelDirection = 0; //This can only show last direction, not if it stopped or not.
     private int mouseX;
     private int mouseY;
 
