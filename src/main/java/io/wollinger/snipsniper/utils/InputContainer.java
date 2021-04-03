3d1f39c92479af8010b34cd573a8f68df92d6ef3
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-04-03 16:53:36 +0200

    Added InputContainer class
    -Used for storing keys, mouse wheel direction and mouse position in a unified place

---
+++ src/main/java/io/wollinger/snipsniper/utils/InputContainer.java
@@ -0,0 +1,38 @@
+package io.wollinger.snipsniper.utils;
+
+public class InputContainer {
+    private boolean[] keys = new boolean[9182];
+    private int mouseWheelDirection = 0; //0 = Not moving
+    private int mouseX;
+    private int mouseY;
+
+    public void setKey(int keyCode, boolean pressed) {
+        keys[keyCode] = pressed;
+    }
+
+    public void setMouseWheelDirection(int direction) {
+        mouseWheelDirection = direction;
+    }
+
+    public void setMousePosition(int mouseX, int mouseY) {
+        this.mouseX = mouseX;
+        this.mouseY = mouseY;
+    }
+
+    public boolean isKeyPressed(int keyCode) {
+        return keys[keyCode];
+    }
+
+    public int getMouseWheelDirection() {
+        return mouseWheelDirection;
+    }
+
+    public int getMouseX() {
+        return mouseX;
+    }
+
+    public int getMouseY() {
+        return mouseY;
+    }
+
+}
