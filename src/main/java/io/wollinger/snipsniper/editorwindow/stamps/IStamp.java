16eed09b3347723a35cc3b3b85d1b829ac4a06c2
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-04-03 16:28:39 +0200

    Added IStamp interface
    -Added IStamp interface. This will make it easier to make multiple kinds of stamps.
    -New "thickness" option has been added, which is optional for things like circles.

---
+++ src/main/java/io/wollinger/snipsniper/editorwindow/stamps/IStamp.java
@@ -0,0 +1,15 @@
+package io.wollinger.snipsniper.editorwindow.stamps;
+
+public interface IStamp {
+    public int getWidth();
+    public int getHeight();
+
+    public int getMinWidth();
+    public int getMinHeight();
+
+    public int getSpeedWidth();
+    public int getSpeedHeight();
+    public int getSpeed();
+
+    public int getThickness();
+}
