fb685270858e4aecdba57f405f6197a28a8e5075
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-04-03 19:31:04 +0200

    Made Utils.copyImage synchronized
    We are trying this out to check if it avoids the white screen bug.

--- src/main/java/io/wollinger/snipsniper/utils/Utils.java
+++ src/main/java/io/wollinger/snipsniper/utils/Utils.java
@@ -71,7 +71,7 @@ public class Utils {
 	    return new Color(Integer.valueOf(colorStr.substring(1, 3), 16), Integer.valueOf( colorStr.substring(3, 5), 16), Integer.valueOf(colorStr.substring(5, 7), 16));
 	}
 
-	public static BufferedImage copyImage(BufferedImage source){
+	public static synchronized BufferedImage copyImage(BufferedImage source){
 	    BufferedImage b = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
 	    Graphics g = b.getGraphics();
 	    g.drawImage(source, 0, 0, source.getWidth(), source.getHeight(), null);
