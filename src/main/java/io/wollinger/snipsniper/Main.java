c825ae57599227edd78624cf0570d2669973da4c
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-04-22 01:53:10 +0200

    Updated Version

--- src/main/java/io/wollinger/snipsniper/Main.java
+++ src/main/java/io/wollinger/snipsniper/Main.java
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.SystemUtils;
 
 public class Main {
 	
-	public final static String VERSION = "20210422_1";
+	public final static String VERSION = "20210422_2";
 	
 	public static String jarFolder = new File("").getAbsolutePath() + "/";
 	public static String mainFolder = jarFolder + "SnipSniper";
