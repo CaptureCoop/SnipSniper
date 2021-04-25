10427e4cd92a1533c6dd93b97720b36e012b0ee8
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-04-25 16:52:09 +0200

    Added vendor

--- buildjpackage.bat
+++ buildjpackage.bat
@@ -2,4 +2,4 @@
 
 set jpackage="C:\Users\Sven\.jdks\adopt-openjdk-14.0.2\bin\jpackage.exe"
 
-%jpackage% --app-version @src/main/resources/version.txt --copyright "2021 Sven Wollinger" --license-file LICENSE --win-dir-chooser --win-menu --win-shortcut --description "SnipSniper" --name "SnipSniper" --dest build/libs/ --input build/libs/ --main-jar SnipSniper.jar --main-class io.wollinger.snipsniper.MainWin --icon src/main/resources/res/SnSn.ico --java-options -Dfile.encoding=GB18030 --add-launcher SnipSniperDebug=jpackagedebug.txt
+%jpackage% --vendor "Sven Wollinger" --app-version @src/main/resources/version.txt --copyright "2021 Sven Wollinger" --license-file LICENSE --win-dir-chooser --win-menu --win-shortcut --description "SnipSniper" --name "SnipSniper" --dest build/libs/ --input build/libs/ --main-jar SnipSniper.jar --main-class io.wollinger.snipsniper.MainWin --icon src/main/resources/res/SnSn.ico --java-options -Dfile.encoding=GB18030 --add-launcher SnipSniperDebug=jpackagedebug.txt
