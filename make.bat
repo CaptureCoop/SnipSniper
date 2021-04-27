efc4e84d732fbde27c1d9f1757bcfa4296a82e05
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-04-27 23:33:49 +0200

    Removed debug exes from main windows build and made them optional in build file.

--- make.bat
+++ make.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 cls
 
 set jpackage="C:\Users\Sven\.jdks\openjdk-16.0.1\bin\jpackage.exe"
@@ -12,11 +13,11 @@ if %1==installer goto create_install
 if %1==jar goto create_jar
 
 :help
-echo Available parameters
+echo Available parameters (<debug> is optional and adds Debug Binaries for console output)
 echo make clean
-echo make full
-echo make portable
-echo make installer
+echo make full <debug>
+echo make portable <debug>
+echo make installer <debug>
 echo make jar
 goto done
 
@@ -34,13 +35,15 @@ goto done
 
 :create_install
 echo Creating installer
-%jpackage% @jpackage_defaults.txt --main-class io.wollinger.snipsniper.main.MainStatic --app-version @src/main/resources/version.txt --type exe --add-launcher SnipSniperDebug=jpackage_static_debug.txt --add-launcher SnipSniperEditor=jpackage_static_editor.txt --license-file LICENSE --win-dir-chooser --win-menu --win-shortcut
+if "%2"=="debug" set debug=--add-launcher SnipSniperDebug=jpackage_static_debug.txt
+%jpackage% @jpackage_defaults.txt --main-class io.wollinger.snipsniper.main.MainStatic --app-version @src/main/resources/version.txt --type exe --add-launcher SnipSniperEditor=jpackage_static_editor.txt %debug% --license-file LICENSE --win-dir-chooser --win-menu --win-shortcut
 rename release\SnipSniper*.exe "SnipSniper_Installer.exe"
 goto :EOF
 
 :create_portable
 echo Creating portable
-%jpackage% @jpackage_defaults.txt --main-class io.wollinger.snipsniper.main.MainPortable --app-version @src/main/resources/version.txt --type app-image --add-launcher SnipSniperDebug=jpackage_portable_debug.txt --add-launcher SnipSniperEditor=jpackage_portable_editor.txt
+if "%2"=="debug" set debug=--add-launcher SnipSniperDebug=jpackage_portable_debug.txt
+%jpackage% @jpackage_defaults.txt --main-class io.wollinger.snipsniper.main.MainPortable --app-version @src/main/resources/version.txt --type app-image --add-launcher SnipSniperEditor=jpackage_portable_editor.txt %debug%
 rename release\SnipSniper "SnipSniper_Portable"
 goto :EOF
 
