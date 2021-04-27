51d89b2db0fae8d4c1b4fa27c18385032fa045e8
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-04-27 16:33:29 +0200

    Code Cleanup

--- src/main/java/io/wollinger/snipsniper/SnipSniper.java
+++ src/main/java/io/wollinger/snipsniper/SnipSniper.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
