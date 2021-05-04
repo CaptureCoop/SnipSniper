8991ef7de88ae205399506563873c211e2088a0e
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-05-04 11:33:15 +0200

    Added simple viewer renderer

---
+++ src/main/java/io/wollinger/snipsniper/viewer/ViewerWindowRender.java
@@ -0,0 +1,17 @@
+package io.wollinger.snipsniper.viewer;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ViewerWindowRender extends JPanel {
+    private ViewerWindow viewerWindow;
+
+    public ViewerWindowRender(ViewerWindow viewerWindow) {
+        this.viewerWindow = viewerWindow;
+    }
+
+    public void paint(Graphics g) {
+        g.drawImage(viewerWindow.getImage(), 0,0, this);
+    }
+
+}
