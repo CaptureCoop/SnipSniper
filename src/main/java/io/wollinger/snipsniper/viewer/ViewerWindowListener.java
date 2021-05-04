c09a9652863b4ef63a3b13185db6101ed25493a0
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-05-04 12:09:16 +0200

    Added listener

---
+++ src/main/java/io/wollinger/snipsniper/viewer/ViewerWindowListener.java
@@ -0,0 +1,31 @@
+package io.wollinger.snipsniper.viewer;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class ViewerWindowListener implements KeyListener {
+    private ViewerWindow viewerWindow;
+
+    public ViewerWindowListener(ViewerWindow viewerWindow) {
+        this.viewerWindow = viewerWindow;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) { }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getKeyCode() == KeyEvent.VK_LEFT) {
+            if(!viewerWindow.isLocked())
+                viewerWindow.slideImage(-1);
+        } else if(keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if(!viewerWindow.isLocked())
+                viewerWindow.slideImage(1);
+        } else if(keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
+            viewerWindow.openEditor();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) { }
+}
