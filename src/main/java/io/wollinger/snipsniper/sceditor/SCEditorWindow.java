e3528f8e96c54710a64cf7c0ee13cf3ad681676b
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-05-08 23:59:50 +0200

    Added positioning and icon to SCEditorWindow

--- src/main/java/io/wollinger/snipsniper/sceditor/SCEditorWindow.java
+++ src/main/java/io/wollinger/snipsniper/sceditor/SCEditorWindow.java
@@ -2,11 +2,31 @@ package io.wollinger.snipsniper.sceditor;
 
 import io.wollinger.snipsniper.Config;
 import io.wollinger.snipsniper.snipscope.SnipScopeWindow;
+import io.wollinger.snipsniper.utils.Icons;
+import io.wollinger.snipsniper.utils.Utils;
 
 import java.awt.image.BufferedImage;
 
 public class SCEditorWindow extends SnipScopeWindow {
+    private final String id;
+    private final Config config;
+
+    final static int X_OFFSET = 8;
+
     public SCEditorWindow(String id, BufferedImage image, int x, int y, String title, Config config, boolean isLeftToRight, String saveLocation, boolean inClipboard, boolean isStandalone) {
         super(image);
+        this.id = id;
+        this.config = config;
+
+        if (isStandalone)
+            setIconImage(Icons.icon_editor);
+        else
+            setIconImage(Icons.icon_taskbar);
+
+        int borderSize = config.getInt("borderSize");
+        if(!isLeftToRight) borderSize = -borderSize;
+        setLocation((x - X_OFFSET) + borderSize, y - getInsets().top + borderSize);
     }
+
+
 }
