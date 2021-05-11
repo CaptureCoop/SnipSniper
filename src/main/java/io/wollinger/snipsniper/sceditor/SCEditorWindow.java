61078aa103df15b2e04bc08a374589a5efeea68b
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-05-11 20:36:20 +0200

    Added correct positioning for editor window

--- src/main/java/io/wollinger/snipsniper/sceditor/SCEditorWindow.java
+++ src/main/java/io/wollinger/snipsniper/sceditor/SCEditorWindow.java
@@ -4,15 +4,20 @@ import io.wollinger.snipsniper.Config;
 import io.wollinger.snipsniper.sceditor.stamps.*;
 import io.wollinger.snipsniper.snipscope.SnipScopeWindow;
 import io.wollinger.snipsniper.utils.Icons;
+import io.wollinger.snipsniper.utils.LogManager;
 import io.wollinger.snipsniper.utils.Utils;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.Map;
+import java.util.logging.Level;
 
 public class SCEditorWindow extends SnipScopeWindow {
     private final String id;
     private final Config config;
+    private final String title;
+    private String saveLocation;
+    private boolean inClipboard;
 
     final static int X_OFFSET = 8;
 
@@ -24,6 +29,9 @@ public class SCEditorWindow extends SnipScopeWindow {
     public SCEditorWindow(String id, BufferedImage image, int x, int y, String title, Config config, boolean isLeftToRight, String saveLocation, boolean inClipboard, boolean isStandalone) {
         this.id = id;
         this.config = config;
+        this.title = title;
+        this.saveLocation = saveLocation;
+        this.inClipboard = inClipboard;
 
         qualityHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         qualityHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
@@ -52,7 +60,46 @@ public class SCEditorWindow extends SnipScopeWindow {
             int borderSize = config.getInt("borderSize");
             if (!isLeftToRight) borderSize = -borderSize;
             setLocation((x - X_OFFSET) + borderSize, y - getInsets().top + borderSize);
+
+            GraphicsEnvironment localGE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            boolean found = false;
+            GraphicsConfiguration bestMonitor = null;
+            final int SAFETY_OFFSET_X = 10 + config.getInt("borderSize"); //This prevents this setup not working if you do a screenshot on the top left, which would cause the location not to be in any bounds
+            for (GraphicsDevice gd : localGE.getScreenDevices()) {
+                for (GraphicsConfiguration graphicsConfiguration : gd.getConfigurations()) {
+                    if(!found) {
+                        Rectangle bounds = graphicsConfiguration.getBounds();
+
+                        Point testLocation = new Point((int) (getLocation().getX() + SAFETY_OFFSET_X), (int) getLocation().getY());
+
+                        if (bounds.contains(testLocation))
+                            found = true;
+
+                        if (testLocation.getX() > bounds.getX() && testLocation.getX() < (bounds.getX() + bounds.getWidth()) && bestMonitor == null) {
+                            bestMonitor = graphicsConfiguration;
+                        }
+                    }
+                }
+            }
+
+            if(!found && bestMonitor != null) {
+                setLocation((int) getLocation().getX(), bestMonitor.getBounds().y);
+                System.out.println("Setting location");
+            }
         }
+        setVisible(true);
+        setSizeAuto();
+    }
+
+    public void refreshTitle() {
+        LogManager.log(id, "Refreshing title", Level.INFO);
+        String newTitle = title;
+        if(saveLocation != null && !saveLocation.isEmpty())
+            newTitle += " (" + saveLocation + ")";
+        if(inClipboard) {
+            newTitle += " (Clipboard)";
+        }
+        setTitle(newTitle);
     }
 
     public static Config getStandaloneEditorConfig() {
