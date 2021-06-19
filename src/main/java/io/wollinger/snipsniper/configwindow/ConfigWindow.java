82b736032d1a874df494852267c3e742073ad98b
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-06-20 01:31:16 +0200

    Only showing panes if needed.

--- src/main/java/io/wollinger/snipsniper/configwindow/ConfigWindow.java
+++ src/main/java/io/wollinger/snipsniper/configwindow/ConfigWindow.java
@@ -9,6 +9,9 @@ import java.awt.*;
 
 public class ConfigWindow extends JFrame {
     private Config config;
+    private JPanel mainConfigPanel;
+    private JPanel editorConfigPanel;
+    private JPanel viewerConfigPanel;
 
     public ConfigWindow(Config config, boolean showMain, boolean showEditor, boolean showViewer) {
         this.config = config;
@@ -16,7 +19,7 @@ public class ConfigWindow extends JFrame {
         setSize(512, 512);
         setTitle(LangManager.getItem("config_label_config"));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //TODO: Mark settings as closed so that settings can only be opened once.
-        setup();
+        setup(showMain, showEditor, showViewer);
         setVisible(true);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -24,22 +27,48 @@ public class ConfigWindow extends JFrame {
     }
 
     //TODO: Make sure that only pages related to from where we opened the config window are enabled.
-    public void setup() {
+    public void setup(boolean showMain, boolean showEditor, boolean showViewer) {
         JTabbedPane tabPane = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
 
-        JPanel mainConfigPanel = new JPanel();
-        JPanel editorConfigPanel = new JPanel();
-        JPanel viewerConfigPanel = new JPanel();
-
-        tabPane.addTab("General Settings", mainConfigPanel);
-        tabPane.addTab("Editor Settings", editorConfigPanel);
-        tabPane.addTab("Viewer Settings", viewerConfigPanel);
         final int iconSize = 16;
-        tabPane.setIconAt(0, new ImageIcon(Icons.icon_taskbar.getScaledInstance(iconSize,iconSize,0)));
-        tabPane.setIconAt(1, new ImageIcon(Icons.icon_editor.getScaledInstance(iconSize,iconSize,0)));
-        tabPane.setIconAt(2, new ImageIcon(Icons.icon_viewer.getScaledInstance(iconSize,iconSize,0)));
+        int index = 0;
+
+        if(showMain) {
+            mainConfigPanel = new JPanel();
+            setupMainPane();
+            tabPane.addTab("General Settings", mainConfigPanel);
+            tabPane.setIconAt(index, new ImageIcon(Icons.icon_taskbar.getScaledInstance(iconSize, iconSize, 0)));
+            index++;
+        }
+
+        if(showEditor) {
+            editorConfigPanel = new JPanel();
+            setupEditorPane();
+            tabPane.addTab("Editor Settings", editorConfigPanel);
+            tabPane.setIconAt(index, new ImageIcon(Icons.icon_editor.getScaledInstance(iconSize,iconSize,0)));
+            index++;
+        }
+
+        if(showViewer) {
+            viewerConfigPanel = new JPanel();
+            setupViewerPane();
+            tabPane.addTab("Viewer Settings", viewerConfigPanel);
+            tabPane.setIconAt(index, new ImageIcon(Icons.icon_viewer.getScaledInstance(iconSize,iconSize,0)));
+        }
 
         add(tabPane);
+    }
+
+    public void setupMainPane() {
+
+    }
+
+    public void setupEditorPane() {
+
+    }
+
+    public void setupViewerPane() {
+
     }
 
 }
