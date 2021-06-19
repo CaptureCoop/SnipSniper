0936e93f1e2eaae8510bd4772b26185e237c4669
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-06-20 01:15:06 +0200

    Added tabs

--- src/main/java/io/wollinger/snipsniper/configwindow/ConfigWindow.java
+++ src/main/java/io/wollinger/snipsniper/configwindow/ConfigWindow.java
@@ -14,11 +14,26 @@ public class ConfigWindow extends JFrame {
 
         setSize(512, 512);
         setTitle(LangManager.getItem("config_label_config"));
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //TODO: Mark settings as closed so that settings can only be opened once.
+        setup();
         setVisible(true);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(screenSize.width / 2 - getWidth() / 2, screenSize.height / 2 - getHeight() / 2);
+    }
+
+    public void setup() {
+        JTabbedPane tabPane = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
+
+        JPanel mainConfigPanel = new JPanel();
+        JPanel editorConfigPanel = new JPanel();
+        JPanel viewerConfigPanel = new JPanel();
+
+        tabPane.addTab("General Settings", mainConfigPanel);
+        tabPane.addTab("Editor Settings", editorConfigPanel);
+        tabPane.addTab("Viewer Settings", viewerConfigPanel);
+
+        add(tabPane);
     }
 
 }
