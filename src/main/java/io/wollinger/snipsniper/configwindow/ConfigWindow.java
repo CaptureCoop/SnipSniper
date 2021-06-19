cd56068a8bc6b0be1fb17601edf2f779a61cb0c9
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-06-20 01:07:22 +0200

    Basic setup

--- src/main/java/io/wollinger/snipsniper/configwindow/ConfigWindow.java
+++ src/main/java/io/wollinger/snipsniper/configwindow/ConfigWindow.java
@@ -1,15 +1,24 @@
 package io.wollinger.snipsniper.configwindow;
 
 import io.wollinger.snipsniper.Config;
+import io.wollinger.snipsniper.utils.LangManager;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ConfigWindow extends JFrame {
     private Config config;
 
     public ConfigWindow(Config config, boolean showMain, boolean showEditor, boolean showViewer) {
         this.config = config;
-        //TODO: Do config stuff
+
+        setSize(512, 512);
+        setTitle(LangManager.getItem("config_label_config"));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(screenSize.width / 2 - getWidth() / 2, screenSize.height / 2 - getHeight() / 2);
     }
 
 }
