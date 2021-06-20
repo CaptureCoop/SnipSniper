e3bc34a86beeebbd1750fb8e79749e04a79dea06
KiwiKaiser <59627902+KiwiKaiser@users.noreply.github.com>
2021-06-20 23:44:42 +0200

    Add config icon
    Add config icon to icon-class

--- src/main/java/io/wollinger/snipsniper/utils/Icons.java
+++ src/main/java/io/wollinger/snipsniper/utils/Icons.java
@@ -12,6 +12,7 @@ public class Icons {
 	public static BufferedImage icon_taskbar;
 	public static BufferedImage icon_editor;
 	public static BufferedImage icon_viewer;
+	public static BufferedImage icon_config;
 	public static BufferedImage[] icons;
 	public static BufferedImage[] alt_icons;
 	
@@ -21,6 +22,7 @@ public class Icons {
 			icon_taskbar = ImageIO.read(SnipSniper.class.getResource("/res/SnSn.png"));
 			icon_editor = ImageIO.read(SnipSniper.class.getResource("/res/SnSnEd.png"));
 			icon_viewer = ImageIO.read(SnipSniper.class.getResource("/res/SnSnVi.png"));
+			icon_config = ImageIO.read(SnipSniper.class.getResource("/res/SnSnCo.png"));
 			icons = new BufferedImage[8];
 			alt_icons = new BufferedImage[8];
 			for(int i = 0; i < 8; i++) {
