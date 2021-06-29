756ef4d606e1cae8af47f61e236feefbb60d990b
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-06-29 19:56:46 +0200

    Added Rendering Hints to Stamp Preview in Config Window

--- src/main/java/io/wollinger/snipsniper/configwindow/StampJPanel.java
+++ src/main/java/io/wollinger/snipsniper/configwindow/StampJPanel.java
@@ -1,5 +1,6 @@
 package io.wollinger.snipsniper.configwindow;
 
+import io.wollinger.snipsniper.sceditor.SCEditorWindow;
 import io.wollinger.snipsniper.sceditor.stamps.IStamp;
 import io.wollinger.snipsniper.utils.Vector2Int;
 
@@ -11,6 +12,7 @@ public class StampJPanel extends JPanel {
     private IStamp stamp;
     private BufferedImage background;
     private boolean backgroundEnabled = true;
+    private RenderingHints qualityHints;
 
     public void setStamp(IStamp stamp) {
         this.stamp = stamp;
@@ -29,16 +31,26 @@ public class StampJPanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
 
+        Graphics2D g2d = (Graphics2D) g;
+
+        if(qualityHints == null) {
+            qualityHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            qualityHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        }
+
+        g2d.setRenderingHints(qualityHints);
+
         if(background != null && backgroundEnabled) {
             if(background.getWidth() >= getWidth() && background.getHeight() >= getHeight()) {
-                g.drawImage(background, 0, 0, getWidth(), getHeight(), 0, 0, getWidth(), getHeight(), null);
+                g2d.drawImage(background, 0, 0, getWidth(), getHeight(), 0, 0, getWidth(), getHeight(), null);
             }
         }
 
-        g.drawRect(0,0,getWidth()-1,getHeight()-1);
+        g2d.drawRect(0,0,getWidth()-1,getHeight()-1);
 
         if(stamp != null)
-            stamp.render(g, null, new Vector2Int(getWidth()/2, getHeight()/2), new Double[]{1D, 1D}, false, false, 0);
+            stamp.render(g2d, null, new Vector2Int(getWidth()/2, getHeight()/2), new Double[]{1D, 1D}, false, false, 0);
+        g2d.dispose();
     }
 
 }
