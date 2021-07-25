ac09a92586c4cee9f1c847f4e61ce3f0b14c89bb
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-07-25 13:24:40 +0200

    Finished setting up Snip Panel

--- src/main/java/io/wollinger/snipsniper/configwindow/ConfigWindow.java
+++ src/main/java/io/wollinger/snipsniper/configwindow/ConfigWindow.java
@@ -218,6 +218,9 @@ public class ConfigWindow extends JFrame {
     public JComponent setupSnipPane(Config configOriginal) {
         snipConfigPanel.removeAll();
 
+        final ColorChooser[] colorChooser = {null};
+        final boolean[] allowSaving = {true};
+
         Config config;
         boolean disablePage = false;
         if (configOriginal != null) {
@@ -228,12 +231,288 @@ public class ConfigWindow extends JFrame {
         }
 
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
         JPanel options = new JPanel(new GridBagLayout());
 
         JComponent dropdown = setupProfileDropdown(options, snipConfigPanel, configOriginal, config, PAGE.snipPanel, indexSnip);
 
+        //BEGIN ELEMENTS
 
+        //BEGIN HOTKEY
+        gbc.gridx = 0;
+        gbc.gridwidth = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = new Insets(0, 10, 0, 10);
+        options.add(createJLabel(LangManager.getItem("config_label_hotkey"), JLabel.RIGHT, JLabel.CENTER), gbc);
+        gbc.gridx = 1;
+        JPanel hotkeyPanel = new JPanel(new GridLayout(0, 2));
+        HotKeyButton hotKeyButton = new HotKeyButton(config.getString(ConfigHelper.PROFILE.hotkey));
+        hotKeyButton.addDoneCapturingListener(e -> {
+            if(hotKeyButton.hotkey != -1) {
+                String hotkeyModifier = "KB";
+                if (!hotKeyButton.isKeyboard)
+                    hotkeyModifier = "M";
+                config.set(ConfigHelper.PROFILE.hotkey, hotkeyModifier + hotKeyButton.hotkey);
+            } else {
+                config.set(ConfigHelper.PROFILE.hotkey, "NONE");
+            }
+        });
+        hotkeyPanel.add(hotKeyButton);
+        JButton deleteHotKey = new JButton(LangManager.getItem("config_label_delete"));
+        deleteHotKey.addActionListener(e -> {
+            hotKeyButton.setText(LangManager.getItem("config_label_none"));
+            hotKeyButton.hotkey = -1;
+            config.set(ConfigHelper.PROFILE.hotkey, "NONE");
+        });
+        hotkeyPanel.add(deleteHotKey);
+        options.add(hotkeyPanel, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END HOTKEY
+
+        //BEGIN SAVEIMAGES
+        gbc.gridx = 0;
+        options.add(createJLabel(LangManager.getItem("config_label_saveimages"), JLabel.RIGHT, JLabel.CENTER), gbc);
+        JCheckBox saveToDisk = new JCheckBox();
+        saveToDisk.setSelected(config.getBool(ConfigHelper.PROFILE.saveToDisk));
+        saveToDisk.addActionListener(e -> config.set(ConfigHelper.PROFILE.saveToDisk, saveToDisk.isSelected() + ""));
+        gbc.gridx = 1;
+        options.add(saveToDisk, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END SAVEIMAGES
+
+        //BEGIN COPYCLIPBOARD
+        gbc.gridx = 0;
+        options.add(createJLabel(LangManager.getItem("config_label_copyclipboard"), JLabel.RIGHT, JLabel.CENTER), gbc);
+        gbc.gridx = 1;
+        JCheckBox copyToClipboard = new JCheckBox();
+        copyToClipboard.setSelected(config.getBool(ConfigHelper.PROFILE.copyToClipboard));
+        copyToClipboard.addActionListener(e -> config.set(ConfigHelper.PROFILE.copyToClipboard, copyToClipboard.isSelected() + ""));
+        options.add(copyToClipboard, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END COPYCLIPBOARD
+
+        //BEGIN BORDERSIZE
+        gbc.gridx = 0;
+        options.add(createJLabel(LangManager.getItem("config_label_bordersize"), JLabel.RIGHT, JLabel.CENTER), gbc);
+        gbc.gridx = 1;
+        JPanel borderSizePanel = new JPanel(new GridLayout(0, 2));
+        JSpinner borderSize = new JSpinner(new SpinnerNumberModel(config.getInt(ConfigHelper.PROFILE.borderSize), 0.0, 999, 1.0)); //TODO: Extend JSpinner class to notify user of too large number
+        borderSize.addChangeListener(e -> config.set(ConfigHelper.PROFILE.borderSize, (int)((double) borderSize.getValue()) + ""));
+        borderSizePanel.add(borderSize);
+        JButton colorBtn = new JButton(LangManager.getItem("config_label_color"));
+        PBRColor borderColor = new PBRColor(config.getColor(ConfigHelper.PROFILE.borderColor));
+        borderColor.addChangeListener(e -> config.set(ConfigHelper.PROFILE.borderColor, Utils.rgb2hex((Color)e.getSource())));
+        colorBtn.setBackground(borderColor.getColor());
+        colorBtn.setForeground(Utils.getContrastColor(borderColor.getColor()));
+        colorBtn.addActionListener(e -> {
+            if(colorChooser[0] == null || !colorChooser[0].isDisplayable()) {
+                int x = (int)((getLocation().getX() + getWidth()/2));
+                int y = (int)((getLocation().getY() + getHeight()/2));
+                colorChooser[0] = new ColorChooser(config, LangManager.getItem("config_label_bordercolor"), borderColor, null, x, y);
+                colorChooser[0].addWindowListener(() -> {
+                    colorBtn.setBackground(borderColor.getColor());
+                    colorBtn.setForeground(Utils.getContrastColor(borderColor.getColor()));
+                });
+            }
+        });
+        borderSizePanel.add(colorBtn, gbc);
+        options.add(borderSizePanel, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END BORDERSIZE
+
+        //BEGIN LOCATION
+        gbc.gridx = 0;
+        options.add(createJLabel(LangManager.getItem("config_label_picturelocation"), JLabel.RIGHT, JLabel.CENTER), gbc);
+        gbc.gridx = 1;
+        JTextField pictureLocation = new JTextField(config.getRawString(ConfigHelper.PROFILE.pictureFolder));
+        pictureLocation.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent focusEvent) {
+                String saveLocationFinal = pictureLocation.getText();
+                if(!saveLocationFinal.endsWith("/"))
+                    saveLocationFinal += "/";
+
+                saveLocationFinal = Utils.replaceVars(saveLocationFinal);
+
+                File saveLocationCheck = new File(saveLocationFinal);
+                if(!saveLocationCheck.exists()) {
+                    allowSaving[0] = false;
+                    Object[] options = {"Okay" , LangManager.getItem("config_sanitation_createdirectory") };
+                    int msgBox = JOptionPane.showOptionDialog(null,LangManager.getItem("config_sanitation_directory_notexist"), LangManager.getItem("config_sanitation_error"), JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+
+                    if(msgBox == 1) {
+                        allowSaving[0] = new File(saveLocationFinal).mkdirs();
+
+                        if(!allowSaving[0]) {
+                            msgError(LangManager.getItem("config_sanitation_failed_createdirectory"));
+                        } else {
+                            config.set(ConfigHelper.PROFILE.pictureFolder, saveLocationFinal);
+                        }
+                    }
+                } else {
+                    allowSaving[0] = true;
+                    config.set(ConfigHelper.PROFILE.pictureFolder, saveLocationFinal);
+                }
+            }
+        });
+        options.add(pictureLocation, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END LOCATION
+
+        //BEGIN SNIPE DELAY
+        gbc.gridx = 0;
+        options.add(createJLabel(LangManager.getItem("config_label_snapdelay"), JLabel.RIGHT, JLabel.CENTER), gbc);
+        gbc.gridx = 1;
+        JSpinner snipeDelay = new JSpinner(new SpinnerNumberModel(config.getInt(ConfigHelper.PROFILE.snipeDelay), 0.0, 100, 1.0));
+        snipeDelay.addChangeListener(e -> config.set(ConfigHelper.PROFILE.snipeDelay, (int)((double) snipeDelay.getValue()) + ""));
+        options.add(snipeDelay, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END SNIPE DELAY
+
+        //BEGIN OPEN EDITOR
+        gbc.gridx = 0;
+        options.add(createJLabel(LangManager.getItem("config_label_openeditor"), JLabel.RIGHT, JLabel.CENTER), gbc);
+        gbc.gridx = 1;
+        JCheckBox openEditor = new JCheckBox();
+        openEditor.setSelected(config.getBool(ConfigHelper.PROFILE.openEditor));
+        openEditor.addActionListener(e -> config.set(ConfigHelper.PROFILE.openEditor, openEditor.isSelected() + ""));
+        options.add(openEditor, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END OPEN EDITOR
+
+        //BEGIN SPYGLASS
+        gbc.gridx = 0;
+        options.add(createJLabel("Use Spyglass", JLabel.RIGHT, JLabel.CENTER), gbc);
+        gbc.gridx = 1;
+
+        JComboBox<Object> spyglassDropdownEnabled = new JComboBox<>(new String[]{"Disabled", "Enabled", "Hold", "Toggle"});
+        JComboBox<Object> spyglassDropdownHotkey = new JComboBox<>(new String[]{"Control", "Shift"});
+        String startMode = config.getString(ConfigHelper.PROFILE.spyglassMode);
+        boolean startEnabled = config.getBool(ConfigHelper.PROFILE.enableSpyglass);
+        spyglassDropdownHotkey.setVisible(false);
+        if(!startMode.equals("none") && startEnabled) {
+            switch(startMode) {
+                case "hold":
+                    spyglassDropdownEnabled.setSelectedIndex(2);
+                    break;
+                case "toggle":
+                    spyglassDropdownEnabled.setSelectedIndex(3);
+                    break;
+            }
+            spyglassDropdownHotkey.setVisible(true);
+        } else if(startMode.equals("none") && startEnabled){
+            spyglassDropdownEnabled.setSelectedIndex(1);
+        } else {
+            spyglassDropdownEnabled.setSelectedIndex(0);
+        }
+
+        switch(config.getInt(ConfigHelper.PROFILE.spyglassHotkey)) {
+            case KeyEvent.VK_CONTROL: spyglassDropdownHotkey.setSelectedIndex(0); break;
+            case KeyEvent.VK_SHIFT: spyglassDropdownHotkey.setSelectedIndex(1); break;
+        }
+        spyglassDropdownEnabled.addItemListener(e -> {
+            boolean enableSpyglass;
+            String spyglassMode;
+            switch(spyglassDropdownEnabled.getSelectedIndex()) {
+                case 0:
+                    spyglassDropdownHotkey.setVisible(false);
+                    enableSpyglass = false; spyglassMode = "none";
+                    break;
+                case 1:
+                    spyglassDropdownHotkey.setVisible(false);
+                    enableSpyglass = true; spyglassMode = "none";
+                    break;
+                case 2:
+                    spyglassDropdownHotkey.setVisible(true);
+                    enableSpyglass = true; spyglassMode = "hold";
+                    break;
+                case 3:
+                    spyglassDropdownHotkey.setVisible(true);
+                    enableSpyglass = true; spyglassMode = "toggle";
+                    break;
+                default:
+                    enableSpyglass = false; spyglassMode = "none";
+            }
+            config.set(ConfigHelper.PROFILE.enableSpyglass, enableSpyglass);
+            config.set(ConfigHelper.PROFILE.spyglassMode, spyglassMode);
+        });
+        spyglassDropdownHotkey.addItemListener(e -> {
+            switch(spyglassDropdownHotkey.getSelectedIndex()) {
+                case 0: config.set(ConfigHelper.PROFILE.spyglassHotkey, KeyEvent.VK_CONTROL); break;
+                case 1: config.set(ConfigHelper.PROFILE.spyglassHotkey, KeyEvent.VK_SHIFT); break;
+            }
+        });
+
+        JPanel spyglassPanel = new JPanel(getGridLayoutWithMargin(0, 2, 0));
+        spyglassPanel.add(spyglassDropdownEnabled);
+        spyglassPanel.add(spyglassDropdownHotkey);
+        options.add(spyglassPanel, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END SPYGLASS
+
+        //BEGIN SPYGLASS ZOOM
+        gbc.gridx = 0;
+        options.add(createJLabel("Spyglass zoom", JLabel.RIGHT, JLabel.CENTER), gbc);
+        gbc.gridx = 1;
+        JComboBox<Object> spyglassZoomDropdown = new JComboBox<>(new String[]{"8x8", "16x16", "32x32", "64x64"});
+        switch(config.getInt(ConfigHelper.PROFILE.spyglassZoom)) {
+            case 8: spyglassZoomDropdown.setSelectedIndex(0); break;
+            case 16: spyglassZoomDropdown.setSelectedIndex(1); break;
+            case 32: spyglassZoomDropdown.setSelectedIndex(2); break;
+            case 64: spyglassZoomDropdown.setSelectedIndex(3); break;
+        }
+
+        spyglassZoomDropdown.addItemListener(e -> {
+            int zoom = 16;
+            switch(spyglassZoomDropdown.getSelectedIndex()) {
+                case 0: zoom = 8; break;
+                case 1: zoom = 16; break;
+                case 2: zoom = 32; break;
+                case 3: zoom = 64; break;
+            }
+            config.set(ConfigHelper.PROFILE.spyglassZoom, zoom);
+        });
+        options.add(spyglassZoomDropdown, gbc);
+        gbc.gridx = 2;
+        options.add(new InfoButton(null), gbc);
+        //END SPYGLASS ZOOM
+        //END ELEMENTS
+
+        //BEGIN SAVE
+        JButton saveAndClose = new JButton("Save and close");
+        saveAndClose.addActionListener(e -> {
+            if(allowSaving[0] && configOriginal != null) {
+                configOriginal.loadFromConfig(config);
+                configOriginal.save();
+                for(CustomWindowListener listener : listeners)
+                    listener.windowClosed();
+                close();
+            }
+        });
+
+        JButton saveButton = new JButton(LangManager.getItem("config_label_save"));
+        saveButton.addActionListener(e -> {
+            if(allowSaving[0] && configOriginal != null) {
+                configOriginal.loadFromConfig(config);
+                configOriginal.save();
+                //This prevents a bug where the other tabs have an outdated config
+                tabPane.setComponentAt(indexEditor, setupEditorPane(configOriginal));
+                tabPane.setComponentAt(indexViewer, setupViewerPane(configOriginal));
+            }
+        });
+
+        gbc.gridx = 0;
+        options.add(saveButton, gbc);
+        gbc.gridx = 1;
+        options.add(saveAndClose, gbc);
+        //END SAVE
 
         snipConfigPanel.add(options);
 
