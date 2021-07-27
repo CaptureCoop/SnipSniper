a26b8c3c180316594c54aa5d65fa547d0a759bd7
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-07-28 00:46:17 +0200

    Changed how we format color, using our own "format" now, formatters are in SSColor

--- src/main/java/io/wollinger/snipsniper/utils/SSColor.java
+++ src/main/java/io/wollinger/snipsniper/utils/SSColor.java
@@ -16,6 +16,10 @@ public class SSColor {
 
 	private final ArrayList<ChangeListener> listeners = new ArrayList<>();
 
+	public SSColor() {
+
+	}
+
 	public SSColor(Color color) {
 		this.primaryColor = color;
 	}
@@ -55,6 +59,14 @@ public class SSColor {
 		}
 	}
 
+	public void setPoint1(Vector2Float point) {
+		point1 = new Vector2Float(point);
+	}
+
+	public void setPoint2(Vector2Float point) {
+		point2 = new Vector2Float(point);
+	}
+
 	public GradientPaint getGradientPaint(int width, int height) {
 		if(secondaryColor == null || point1 == null || point2 == null) {
 			LogManager.log("SSColor", "A variable wasnt set as needed. Status: " + this, Level.SEVERE);
@@ -68,6 +80,67 @@ public class SSColor {
 
 	public void addChangeListener(ChangeListener listener) {
 		listeners.add(listener);
+	}
+
+	public String toSaveString() {
+		String string = Utils.rgb2hex(primaryColor);
+		if(primaryColor.getAlpha() != 255)
+			string += "_a" + primaryColor.getAlpha();
+
+		if(point1 != null)
+			string += "_x" + point1.getX() + "_y" + point1.getY();
+
+		if(secondaryColor != null || point2 != null)
+			string += "___";
+
+		if(secondaryColor != null) {
+			string += Utils.rgb2hex(secondaryColor);
+			if(secondaryColor.getAlpha() != 255)
+				string += "_a" + secondaryColor.getAlpha();
+		}
+
+		if (point2 != null)
+			string += "_x" + point2.getX() + "_y" + point2.getY();
+		return string;
+	}
+
+	public static SSColor fromSaveString(String string) {
+		SSColor newColor = new SSColor();
+		int index = 0;
+		for(String part : string.split("___")) {
+			int alpha = -1;
+			Color color = null;
+
+			float defaultPos = 0;
+			if(index != 0) defaultPos = 1;
+			Vector2Float pos = new Vector2Float(defaultPos, defaultPos);
+
+			for(String str : part.split("_")) {
+				switch(str.charAt(0)) {
+					case '#': color = Utils.hex2rgb(str); break;
+					case 'a': alpha = Integer.parseInt(str.substring(1)); break;
+					case 'x': pos.setX(Float.parseFloat(str.substring(1))); break;
+					case 'y': pos.setY(Float.parseFloat(str.substring(1))); break;
+				}
+
+				if(alpha == -1 && color != null)
+					alpha = color.getAlpha();
+
+				if(color != null) {
+					if(index == 0)
+						newColor.primaryColor = new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
+					else if(index == 1)
+						newColor.secondaryColor = new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
+					newColor.point1 = pos;
+				}
+
+				if(index == 0) newColor.point1 = pos;
+				else if(index == 1) newColor.point2 = pos;
+			}
+			index++;
+		}
+
+		return newColor;
 	}
 
 	public String toString() {
