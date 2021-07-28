a12e1cd2d56e4a998a5239f6111e98740d0e04fb
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-07-28 14:10:01 +0200

    first working setup for new color chooser

--- src/main/java/io/wollinger/snipsniper/colorchooser/ColorChooserGradient.java
+++ src/main/java/io/wollinger/snipsniper/colorchooser/ColorChooserGradient.java
@@ -1,23 +1,21 @@
 package io.wollinger.snipsniper.colorchooser;
 
+import io.wollinger.snipsniper.utils.SSColor;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class ColorChooserSingle extends JPanel {
-    private Color color;
+public class ColorChooserGradient extends JPanel {
+    private SSColor color;
 
-    public ColorChooserSingle(ColorChooserPreviewPanel previewPanel) {
-
-    }
-
-    public void setColor(Color color) {
+    public void setColor(SSColor color) {
         this.color = color;
     }
 
     @Override
     public void paint(Graphics g) {
         if(color != null) {
-            g.setColor(color);
+            g.setColor(color.getPrimaryColor());
             g.fillRect(getWidth() / 2 - getHeight()/2, 0, getHeight(), getHeight());
         }
     }
--- src/main/java/io/wollinger/snipsniper/colorchooser/ColorChooserPreviewPanel.java
+++ src/main/java/io/wollinger/snipsniper/colorchooser/ColorChooserPreviewPanel.java
@@ -1,16 +1,28 @@
 package io.wollinger.snipsniper.colorchooser;
 
+import io.wollinger.snipsniper.utils.SSColor;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class ColorChooserPreviewPanel extends JPanel {
+    ColorChooserSingle panelSingle;
+    ColorChooserGradient panelGradient;
+    JTabbedPane tabPane;
+
     public ColorChooserPreviewPanel(ColorChooser colorChooser) {
-        setPreferredSize(new Dimension(colorChooser.getWidth(), 256));
-        JTabbedPane tabPane = new JTabbedPane(JTabbedPane.TOP);
-        ColorChooserSingle panelSingle = new ColorChooserSingle(colorChooser.getWidth(), 256);
+        setPreferredSize(new Dimension(0, 256));
+        setLayout(new GridLayout());
+        tabPane = new JTabbedPane(JTabbedPane.TOP);
+
+        panelSingle = new ColorChooserSingle(this);
         panelSingle.setColor(Color.RED);
+
+        panelGradient = new ColorChooserGradient();
+        panelGradient.setColor(new SSColor(Color.BLUE));
+
         tabPane.addTab("Single color", panelSingle);
-        tabPane.addTab("Gradient",  new JPanel());
+        tabPane.addTab("Gradient",  panelGradient);
         add(tabPane);
     }
 
