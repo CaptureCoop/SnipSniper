d3d363c20a3c07795079077dc8a90f1ac64c48d3
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-09-13 09:02:16 +0200

    Added equals method to version class

--- src/test/java/org/snipsniper/VersionTest.java
+++ src/test/java/org/snipsniper/VersionTest.java
@@ -10,5 +10,8 @@ public class VersionTest {
     public void test() {
         assertTrue(new Version("1.0.0").isNewerThan(new Version("0.9.5")));
         assertFalse(new Version("1.5.10").isNewerThan(new Version("1.5.10")));
+
+        assertTrue(new Version("10.0.57").equals(new Version("10.0.57")));
+        assertFalse(new Version("10.0.57").equals(new Version("10.3.57")));
     }
 }
