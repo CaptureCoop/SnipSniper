56e16e97a6793e4133644849b7eaa891baa3a53f
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-09-24 11:05:13 +0200

    Changed to hashmap

--- src/main/java/org/snipsniper/utils/DropdownItemRenderer.java
+++ src/main/java/org/snipsniper/utils/DropdownItemRenderer.java
@@ -7,20 +7,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class DropdownItemRenderer extends DefaultListCellRenderer {
-    DropdownItem[] items;
-    private int lastSelectedIndex = 0;
+    private HashMap<String, Icon> images = new HashMap<>();
+
     public DropdownItemRenderer(DropdownItem[] items) {
-        this.items = items;
+        for(DropdownItem item : items)
+            images.put(item.getID(), item.getIcon());
     }
 
     @Override
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-        if(index == -1) index = lastSelectedIndex;
-        if(index >= 0)
-            label.setIcon(items[index].getIcon());
-        if(isSelected && index != -1) lastSelectedIndex = index;
-        System.out.println(value);
+        DropdownItem item = (DropdownItem) value;
+        label.setIcon(images.get(item.getID()));
         return label;
     }
 }
