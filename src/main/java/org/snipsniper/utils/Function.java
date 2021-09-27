5864a68135f5a7799e3083ad9b2c909dcc7368a5
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-09-27 07:40:16 +0200

    Added Function class

---
+++ src/main/java/org/snipsniper/utils/Function.java
@@ -0,0 +1,6 @@
+package org.snipsniper.utils;
+
+public abstract class Function {
+    public boolean run(String... args) { return true; }
+    public boolean run(Object... args) { return true; }
+}
