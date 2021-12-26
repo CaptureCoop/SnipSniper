d9f5191e6d9a31b28415c822d33f0ae530c66132
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-12-26 03:04:05 +0100

    Added rendered bar, not functional yet

--- src/main/java/org/snipsniper/colorchooser/SSColorChooserHueBar.java
+++ src/main/java/org/snipsniper/colorchooser/SSColorChooserHueBar.java
@@ -5,28 +5,86 @@ import org.snipsniper.utils.SSColor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
 
 public class SSColorChooserHueBar extends JPanel {
     private SSColor color;
     private float position;
-    private DrawUtils.DIRECTION direction;
+    private final DrawUtils.DIRECTION direction;
 
     private final static int MARGIN = 10;
+    private final static int SEL_MARGIN = 4;
+    private final static int SEL_MARGIN_OFF = 2;
+    private boolean hasGrabbed = false;
 
     public SSColorChooserHueBar(SSColor color, DrawUtils.DIRECTION direction) {
         this.color = color;
         this.direction = direction;
+        updateHSV();
+        color.addChangeListener(e -> updateHSV());
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+                int mouseX = (int)mouseEvent.getPoint().getX();
+                int mouseY = (int)mouseEvent.getPoint().getY();
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent mouseEvent) {
+
+            }
+        });
+    }
+
+    private void updateHSV() {
+        float[] hsv = new float[3];
+        Color.RGBtoHSB(color.getPrimaryColor().getRed(), color.getPrimaryColor().getBlue(), color.getPrimaryColor().getGreen(), hsv);
+        position = hsv[2];
     }
 
     @Override
     public void paint(Graphics g) {
-        float[] hsv = new float[3];
-        Color.RGBtoHSB(color.getPrimaryColor().getRed(), color.getPrimaryColor().getBlue(), color.getPrimaryColor().getGreen(), hsv);
         int sizeX = getWidth() - MARGIN;
         int sizeY = getHeight() - MARGIN;
         g.drawImage(DrawUtils.createHSVHueBar(sizeX, sizeY, direction), MARGIN / 2, MARGIN / 2, sizeX, sizeY, this);
         g.setColor(Color.BLACK);
         g.drawRect(MARGIN / 2 - 1, MARGIN / 2 - 1, sizeX + 1, sizeY + 1);
+        g.setColor(Color.GRAY);
+        switch(direction) {
+            case VERTICAL:
+                int yPos = (int) (sizeY / (1 / position)) + MARGIN / 2;
+                g.fillRect(SEL_MARGIN_OFF, yPos - SEL_MARGIN, getWidth() - SEL_MARGIN_OFF * 2, SEL_MARGIN * 2);
+                break;
+            case HORIZONTAL:
+                int xPos = (int) (sizeX / (1 / position)) + MARGIN / 2;
+                g.fillRect(xPos - SEL_MARGIN, SEL_MARGIN_OFF, SEL_MARGIN * 2, getHeight() - SEL_MARGIN_OFF * 2);
+                break;
+        }
+
     }
 
 }
