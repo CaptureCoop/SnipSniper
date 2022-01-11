cfbf6aaab95b247dcd5fd790054fa9bc69c18f41
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-01-11 13:59:29 +0100

    Enhanced jvm-creator make script

--- utils/jvm-creator/make.bat
+++ utils/jvm-creator/make.bat
@@ -1,5 +1,19 @@
 @echo off
 
+IF "%~1" == "" goto help
+
+if %1==help goto help
+if %1==clean goto clean
+if %1==build goto build
+
+:help
+echo JVM-Creator
+echo Available parameters
+echo make clean
+echo make build
+goto exit
+
+:build
 if not exist jdk\ (
 	echo jdk folder not found, please place as "jdk"
 	goto exit
@@ -26,5 +40,16 @@ echo Running jlink with powerjdep-output.txt...
 jdk\bin\jlink.exe --output output\jdk\ --add-modules %modules%
 
 echo Done!
+goto exit
+
+:clean
+echo Cleaning files...
+if exist output\ rmdir /Q /S output
+if exist jdk\ rmdir /Q /S jdk
+if exist SnipSniper.jar del /Q /S SnipSniper.jar >> nul
+if exist powerjdep-output.txt del /Q /S powerjdep-output.txt >> nul
+if exist jdeps-output.txt del /Q /S jdeps-output.txt >> nul
+echo Done!
+goto exit
 
 :exit
