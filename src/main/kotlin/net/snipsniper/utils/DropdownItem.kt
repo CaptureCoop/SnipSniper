0a0a0937b65f4f726d8a1d30e1feab00222146a0
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-09-13 10:37:46 +0200

    Port: DropdownItemRenderer.java -> DropdownItem.kt

--- src/main/kotlin/net/snipsniper/utils/DropdownItem.kt
+++ src/main/kotlin/net/snipsniper/utils/DropdownItem.kt
@@ -1,9 +1,8 @@
 package net.snipsniper.utils
 
+import java.awt.Component
 import java.awt.Image
-import javax.swing.Icon
-import javax.swing.ImageIcon
-import javax.swing.JComboBox
+import javax.swing.*
 
 class DropdownItem(private val label: String, val id: String) {
     lateinit var icon: Icon
@@ -15,7 +14,6 @@ class DropdownItem(private val label: String, val id: String) {
 
     fun compare(other: DropdownItem) = compare(other.id)
     fun compare(id: String): Boolean = this.id == id
-
     override fun toString(): String = label
 
     companion object {
@@ -28,5 +26,20 @@ class DropdownItem(private val label: String, val id: String) {
             }
             return 0
         }
+    }
+}
+
+class DropdownItemRenderer(items: Array<DropdownItem>): DefaultListCellRenderer() {
+    private val images = HashMap<String, Icon>()
+
+    init {
+        items.forEach { images[it.id] = it.icon }
+    }
+
+    override fun getListCellRendererComponent(list: JList<*>?, value: Any?, index: Int, isSelected: Boolean, cellHasFocus: Boolean): Component {
+        val label = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus) as JLabel
+        val item = value as DropdownItem
+        label.icon = item.icon
+        return label
     }
 }
