6ed4008d595aae1f9a773b7b70d349aa0d9dba2f
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-09-14 12:15:39 +0200

    Generate missing texture image in code

--- src/main/kotlin/net/snipsniper/ImageManager.kt
+++ src/main/kotlin/net/snipsniper/ImageManager.kt
@@ -4,6 +4,7 @@ import net.snipsniper.config.ConfigHelper
 import net.snipsniper.utils.FileUtils
 import org.capturecoop.cclogger.CCLogger
 import org.json.JSONArray
+import java.awt.Color
 import java.awt.Image
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
@@ -15,6 +16,7 @@ class ImageManager {
         private val images = HashMap<String, BufferedImage>()
         private val animatedImages = HashMap<String, Image>()
         private lateinit var filenameList: Array<String>
+        private val missingImg = getErrorImage()
 
         fun loadResources() {
             CCLogger.log("Loading images...")
@@ -40,15 +42,33 @@ class ImageManager {
 
         fun getListAsString(): Array<String> = filenameList
 
-        fun getImage(path: String): BufferedImage? = images[path] ?: images["missing.png"].also { CCLogger.log("Could not find image under path $path") }
-        fun getAnimatedImage(path: String): Image? = animatedImages[path] ?: images["missing.png"].also { CCLogger.log("Could not find image under path $path") }
+        fun getImage(path: String): BufferedImage = images[path] ?: missingImg.also { CCLogger.log("Could not find image under path $path") }
+        fun getAnimatedImage(path: String): Image = animatedImages[path] ?: missingImg.also { CCLogger.log("Could not find image under path $path") }
         fun hasImage(path: String): Boolean = animatedImages.containsKey(path) || images.containsKey(path)
 
-        fun getCodePreview(): BufferedImage? {
+        fun getCodePreview(): BufferedImage {
             return when(SnipSniper.config.getString(ConfigHelper.MAIN.theme)) {
-                "dark" -> images["preview/code_dark.png"]
-                else -> images["preview/code_light.png"]
+                "dark" -> images["preview/code_dark.png"] ?: missingImg
+                "light" -> images["preview/code_light.png"] ?: missingImg
+                else -> missingImg
             }
+        }
+
+        private fun getErrorImage(): BufferedImage {
+            val image = BufferedImage(8, 8, BufferedImage.TYPE_INT_RGB)
+            val g = image.createGraphics()
+            var printBlack = true
+            val pinkColor = Color(255, 1, 254)
+            for(y in 0 until 8) {
+                for(x in 0 until 8) {
+                    printBlack = !printBlack
+                    g.color = if(printBlack) Color.BLACK else pinkColor
+                    g.drawLine(x, y, x, y)
+                }
+                printBlack = !printBlack
+            }
+            g.dispose()
+            return image
         }
     }
 }
