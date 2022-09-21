103d329bf1e259055b22760c3836dd1e2edbc75e
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-09-21 21:20:17 +0200

    Add BufferedImage.clone()

--- src/main/kotlin/net/snipsniper/utils/Extensions.kt
+++ src/main/kotlin/net/snipsniper/utils/Extensions.kt
@@ -18,6 +18,7 @@ fun BufferedImage.ensureAlphaLayer() = ImageUtils.ensureAlphaLayer(this)
 fun BufferedImage.rotateClockwise90() = ImageUtils.rotateClockwise90(this)
 fun BufferedImage.rotateClockwise90(times: Int) = ImageUtils.rotateClockwise90(this, times)
 fun BufferedImage.trim(): BufferedImage = ImageUtils.trimImage(this)
+fun BufferedImage.clone(): BufferedImage = ImageUtils.copyImage(this)
 
 fun Image.scaled(width: Int, height: Int, hints: Int): Image = this.getScaledInstance(width, height, hints)
 fun Image.scaled(width: Int, height: Int): Image = this.scaled(width, height, Image.SCALE_DEFAULT)
