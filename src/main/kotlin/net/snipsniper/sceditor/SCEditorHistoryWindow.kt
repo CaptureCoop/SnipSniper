17a944f3ddf60a3df0063b0d70fce7f948429843
SvenWollinger <accounts@wollinger.email>
2022-11-21 09:52:56 +0100

    set title and location

--- src/main/kotlin/net/snipsniper/sceditor/SCEditorHistoryWindow.kt
+++ src/main/kotlin/net/snipsniper/sceditor/SCEditorHistoryWindow.kt
@@ -2,7 +2,9 @@ package net.snipsniper.sceditor
 
 import net.snipsniper.utils.getImage
 import org.capturecoop.ccutils.utils.CCIClosable
+import java.awt.Dimension
 import java.awt.GridBagLayout
+import java.awt.Point
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
 import javax.swing.JFrame
@@ -14,9 +16,14 @@ class SCEditorHistoryWindow(private val editor: SCEditorWindow): JFrame(), CCICl
         defaultCloseOperation = DO_NOTHING_ON_CLOSE
         layout = GridBagLayout()
         iconImage = "icons/questionmark.png".getImage()
+        title = "Edit History"
         addWindowListener(object: WindowAdapter() {
             override fun windowClosing(e: WindowEvent) { close() }
         })
+        size = Dimension(512, 756)
+        editor.location.also {
+            location = Point(it.x + editor.width / 2 - width / 2, it.y + editor.height / 2 - height / 2)
+        }
         isVisible = true
     }
 
