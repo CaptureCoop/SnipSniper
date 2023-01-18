683a1acda1f4b5d9d28c9b875b1cbcb454f0813d
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2023-01-18 09:36:32 +0100

    Remove test code

--- src/main/kotlin/net/snipsniper/utils/Function.kt
+++ src/main/kotlin/net/snipsniper/utils/Function.kt
@@ -6,18 +6,5 @@ abstract class Function {
     //open fun run(vararg args: Any?): Boolean = true
     open fun run(vararg args: Boolean): Boolean = true
     open fun run(vararg args: Int): Boolean = true //TODO: Change to Int after portjob is complete
-    open fun run(vararg args: ConfigSaveButtonState): Boolean = true
     open fun run(state: ConfigSaveButtonState): Boolean = true
-}
-
-
-
-fun baba(action: (String) -> (String)) {
-    println("Action invoked: ${action.invoke("baba")}")
-}
-
-fun main() {
-    baba {
-        return@baba "hello"
-    }
 }
