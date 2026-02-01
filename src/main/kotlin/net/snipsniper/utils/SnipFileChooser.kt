b1ed7b36e5401015ed7fbf8e2194e014c779647e
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2026-02-01 12:25:06 +0100

    Create SnipFileChooser Utility Object

---
+++ src/main/kotlin/net/snipsniper/utils/SnipFileChooser.kt
@@ -0,0 +1,40 @@
+package net.snipsniper.utils
+
+import com.formdev.flatlaf.util.SystemFileChooser
+import java.awt.Component
+import java.io.File
+import javax.swing.JComponent
+import javax.swing.JFileChooser
+
+object SnipFileChooser {
+    data class Filter(
+        val description: String,
+        val extensions: List<String>
+    )
+    enum class SelectionType(val swingType: Int, val nativeType: Int) {
+        FILES_ONLY(JFileChooser.FILES_ONLY, SystemFileChooser.FILES_ONLY),
+        DIRECTORY_ONLY(JFileChooser.DIRECTORIES_ONLY, SystemFileChooser.DIRECTORIES_ONLY),
+        FILES_AND_DIRECTORIES(JFileChooser.FILES_AND_DIRECTORIES, SystemFileChooser.DIRECTORIES_ONLY)
+    }
+
+    fun openSystemFileChooser(
+        path: String = File("").absolutePath,
+        multiFileSelection: Boolean = false,
+        parent: Component? = null,
+        type: SelectionType = SelectionType.FILES_ONLY,
+        fileFilters: List<Filter> = listOf()
+    ): Array<File>? {
+        val chooser = SystemFileChooser()
+        chooser.currentDirectory = File(path)
+        chooser.isMultiSelectionEnabled = multiFileSelection
+        chooser.fileSelectionMode = type.nativeType
+        fileFilters.forEach { filter ->
+            chooser.addChoosableFileFilter(SystemFileChooser.FileNameExtensionFilter(filter.description, *filter.extensions.toTypedArray()))
+        }
+        if (chooser.showOpenDialog(parent) == SystemFileChooser.APPROVE_OPTION) {
+            //TODO: is this neccessary? can we always return selectedFiles?
+            return if(multiFileSelection) chooser.selectedFiles else arrayOf(chooser.selectedFile)
+        }
+        return null
+    }
+}
